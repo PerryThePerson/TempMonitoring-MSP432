e25040e0bca74e6b836918dca8df95bd5bb98650
Christopher Perry <chrisleeperry@gmail.com>
2017-04-24 11:51:47 -0400

    added email functionality to serial script

--- readSerial.py
+++ readSerial.py
@@ -1,6 +1,22 @@
 import serial
-import time
-ser = serial.Serial('COM6', 9600, timeout=0)
+import os
+import smtplib
 
-s=ser.read(100)
-print(s)
+ser = serial.Serial('COM3', 9600)
+while (1):
+	
+	s = ser.readline().rstrip()
+
+	print(s)
+	if s == b'Shut down':
+	
+		os.system("shutdown -s")
+		
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login('Email#', 'Password#')
+		server.sendmail('Email#', 'Number#', 'Message#')
+		break
+	
+	
+print("Shutting down system...")
